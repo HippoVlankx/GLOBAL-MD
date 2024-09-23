23e6100d544f4acfd97d8709bd44ee40974b10ff
HippoVlankx <vlankxhippo@gmail.com>
2024-09-23 22:15:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,9 +9,9 @@ global.socialm = "GitHub: GlobalTechInfo" //ur github or insta name
 global.location = "Pakistan, Karachi, Karachi" //ur location
 
 //new
-global.botname = '© 𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur bot name
-global.ownernumber = ['923444844060'] //ur owner number, dont add more than one
-global.ownername = '© GlobalTechInfo' //ur owner name
+global.botname = '© POLAND-BOT' //ur bot name
+global.ownernumber = ['48729752305'] //ur owner number, dont add more than one
+global.ownername = '© Polandski' //ur owner name
 global.websitex = "https://www.youtube.com/@GlobalTechInfo"
 global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
 global.themeemoji = '🪀'
@@ -21,7 +21,7 @@ global.packname = "Sticker By"
 global.author = "GlobalTechInfo\n\n+923444844060"
 global.creator = "923444844060@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923444844060"] // Premium User
+global.premium = ["48729752305"] // Premium User
 
 //channel id
 global.xchannel = {
